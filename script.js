6bc5c7c432eae1f8a70e2fd95cbe704207ae20ee
Kostas Chikimtzis <kostaschikis@gmail.com>
2019-08-13 22:11:27 +0300

    Chat With Usernames Working

--- script.js
+++ script.js
@@ -3,7 +3,61 @@
 // Connect to the server
 const socket = io('http://localhost:3000');
 
-// data = 'hello world' | coming from the server
+// Get Message Container 
+const messageContainer = document.getElementById('message-container');
+// Get Message-Form from DOM
+const messageForm = document.getElementById('send-container');
+const messageInput = document.getElementById('message-input');
+
+// Ask for Username & Show 'Joined' Message 
+let name = prompt('What is your name');
+if (name == null) {
+    do { 
+        alert('Warning: Your name is null');
+        name = prompt('What is your name');
+    } while (name == null)
+}
+appendMessage('You Joined the Chat, have fun 💯');
+
+// Send the new user name to the server
+socket.emit('new-user', name);
+
+// Receive Message -> data coming from the server
 socket.on('chat-message', data => {
-    console.log(data);
+    // Append it to DOM
+    appendMessage(`${data.name}: ${data.message}`);
 });
+
+// Receive New User -> data coming from the server
+socket.on('user-connected', data => {
+    // Append it to DOM
+    appendMessage(`${data} connected to the chat 💻`);
+});
+
+// Receive Disconnected User -> data coming from the server
+socket.on('user-disconnected', data => {
+    // Append it to DOM
+    appendMessage(`${data} disconnected from the chat 👋`);
+});
+
+messageForm.addEventListener('submit', () => {
+    // Prevent from reloding the page 
+    event.preventDefault();
+    // Get form imput value
+    const message = messageInput.value;
+    // Send it to the Server
+    socket.emit('send-chat-message', message);
+    // Empty the input field
+    messageInput.value = '';
+    // Show your message in DOM
+    appendMessage(`You: ${message}`);
+});
+
+// Add the message into DOM
+function appendMessage(message) {
+    // Create a new div element
+    const messageElement = document.createElement('div');
+    messageElement.innerText = message;
+    // Append the new div into the message container
+    messageContainer.append(messageElement);
+}
--- server.js
+++ server.js
@@ -3,8 +3,28 @@
 // Require socket.io
 const io = require('socket.io')(3000);
 
+const users = {};
+
 // When a new user reaches the server -> send 'hello world' to client
 io.on('connection', socket => {
-    console.log('new user');
-    socket.emit('chat-message', 'Hello World');
+
+    // New User Handler | 'new-user' event
+    socket.on('new-user', name => {
+        users[socket.id] = name;
+        // Send to everyone who connected | 'user-connected' event
+        socket.broadcast.emit('user-connected', name);
+    }); 
+
+    // Receive the message | 'send-chat-message' event
+    socket.on('send-chat-message', message => {
+        // Send the message to everyone connected | 'chat-message' event
+        socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] });  
+    });
+
+    // Disconnect Handler | 'disconnect' event
+    socket.on('disconnect', () => {
+        // Send who disconnect | 'user-disconnected' event
+        socket.broadcast.emit('user-disconnected', users[socket.id]);
+        delete users[socket.id];
+    });
 });
